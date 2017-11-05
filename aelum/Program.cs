b9bc49e26661c83ca32cec4c53d9767d31ed6662
Alan Q <alanqdev@gmail.com>
2017-11-05 14:39:04 -0200

    laying groundwork

---
+++ aelum/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    }
+}
+
+
+
+class Engine
+{
+    public static Scene scene;
+}
+
+class Scene
+{
+    private List<Node> nodes;
+    private List<PluginSystemUntyped> systems;
+}
+
+abstract class PluginSystemUntyped
+{
+    
+}
+
+abstract class PluginSystem<T> : PluginSystemUntyped where T : Plugin
+{
+    private List<T> plugins;
+}
+
+class Node
+{
+    private List<Plugin> plugins;
+}
+
+abstract class Plugin
+{
+    private Node node;
+}
+
+
+
