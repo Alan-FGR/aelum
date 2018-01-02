c9df6688565dec279a844d2ca779e987f05c7b0d
Alan <alanqdev@gmail.com>
2018-01-01 23:54:11 -0200

    no message

--- aelum2/Program.cs
+++ aelum2/Program.cs
@@ -12,16 +12,46 @@ class Core
 class Scene
 {
    private List<Node> nodes_;
+   private List<ComponentSystem> systems_;
+
+   public T GetSystem<T>() where T : ComponentSystem
+   {
+      foreach (ComponentSystem system in systems_)
+         if (system is T)
+            return system as T;
+      return null;
+   }
+
+   public T AddSystem<T>() where T : ComponentSystem, new()
+   {
+      T system = new T();
+      systems_.Add(system);
+      return system;
+   }
+
 }
 
 class Node
+{
+   private Scene scene_;
+   private List<WeakReference<Component>> componentsWeakRefs_;
+
+   public T AddComponent<T>() where T : Component, new()
+   {
+      
+   }
+
+}
+
+class ComponentSystem
 {
    private List<Component> components_;
 }
 
 class Component
 {
-   
+   private ComponentSystem system_;
+   private Node node_;
 }
 
 class Program
